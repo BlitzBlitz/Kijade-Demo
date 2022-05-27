92399be582d951fc27e53571b1215097bcf2bf9b
Klement Binaj <klementbinaj@gmail.com>
2022-05-27 14:34:11 +0300

    hulasa Done

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let linksContainer = document.querySelectorAll(".side-links")[0];
 let skillsLocations = [
   "m/عرض - الاستراتيجية الشخصية.pdf",
   "m/عرض - القبعات الست.pdf",
@@ -17,23 +16,87 @@ let skillsLocations = [
   "m/مهارات التواصل.pdf",
   "m/مهارات سكامبر.pdf",
 ];
+let hulasaLocation = [
+  "الخلاصات/اعادة اختراع القيادة.pdf",
+  "الخلاصات/الاستعداد_الذكي_للمستقبل.pdf",
+  "الخلاصات/البديل_الثالث.pdf",
+  "الخلاصات/التفكير_الإبداعي.pdf",
+  "الخلاصات/الذكاء الوجداني.pdf",
+  "الخلاصات/السلطة.pdf",
+  "الخلاصات/العظمة.pdf",
+  "الخلاصات/القيادة  من  الداخل.pdf",
+  "الخلاصات/القيادة في وقت الازمات.pdf",
+  "الخلاصات/القيادة وقت الأزمات.pdf",
+  "الخلاصات/تعاليم_“بيتر_دراكر”.pdf",
+  "الخلاصات/تلخيص-كتاب-21-قانونا-لا-يحمل-الجدل-في-القيادة.pdf",
+  "الخلاصات/قوة التغيير.pdf",
+  "الخلاصات/قيادة التغيير  خطة عمل من ثمان خطوات.pdf",
+  "الخلاصات/كبسولات القيادة.pdf",
+  "الخلاصات/من_طالب_إلى_قائد.pdf",
+  "الخلاصات/وصفات جاك ويلش للقيادة.pdf",
+];
+let kutubLocation = [];
+let mekatiLocation = [];
+let categoriesNames = ["المهارات", "الكتب", "الخلاصات", "المقاطع"];
 
-for (let i = 0; i < skillsLocations.length; i++) {
-  let link = document.createElement("a");
-  link.classList.add("side-link");
-  if (i == 0) {
-    link.classList.add("active");
+let linksContainer = document.querySelectorAll(".side-links")[0];
+let categoryContainer = document.querySelectorAll(".navbar")[0];
+
+displayCategories(categoriesNames);
+displayItems(skillsLocations);
+
+function displayCategories(categories) {
+  for (let i = 0; i < categories.length; i++) {
+    let category = document.createElement("a");
+    category.classList.add("nav-link");
+    if (i == 0) {
+      category.classList.add("active-green");
+    }
+    category.href = "#";
+    category.innerHTML = categories[i];
+    category.addEventListener("click", function () {
+      let current = document.querySelector(".active-green");
+      current.classList.remove("active-green");
+      this.classList.add("active-green");
+      console.log(this.innerText);
+      switch (this.innerText) {
+        case "المهارات":
+          displayItems(skillsLocations);
+          break;
+        case "الكتب":
+          displayItems(kutubLocation);
+          break;
+        case "الخلاصات":
+          displayItems(hulasaLocation);
+          break;
+        case "المقاطع":
+          displayItems(mekatiLocation);
+        default:
+          break;
+      }
+    });
+    categoryContainer.appendChild(category);
   }
-  link.href = "#";
-  link.innerHTML = skillsLocations[i].slice(2).split(".")[0];
-  link.addEventListener("click", function () {
-    let current = document.querySelector(".active");
-    current.classList.remove("active");
-    this.classList.add("active");
-    let pdf = document.querySelector("#player");
-    pdf.src = skillsLocations[i];
-    console.log(1);
-  });
-  linksContainer.appendChild(link);
 }
-console.log(linksContainer.childNodes);
+
+function displayItems(items) {
+  linksContainer.innerHTML = "";
+  for (let i = 0; i < items.length; i++) {
+    let link = document.createElement("a");
+    link.classList.add("side-link");
+    if (i == 0) {
+      link.classList.add("active");
+    }
+    link.href = "#";
+    link.innerHTML = items[i].split("/")[1].split(".")[0];
+    link.addEventListener("click", function () {
+      let current = document.querySelector(".active");
+      current.classList.remove("active");
+      this.classList.add("active");
+      let pdf = document.querySelector("#player");
+      pdf.src = items[i];
+    });
+    linksContainer.appendChild(link);
+  }
+  console.log(linksContainer.childNodes);
+}
